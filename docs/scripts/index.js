3c4a4913acc53d51afd1766f07eaf3a5f464c762
Mohammed Yaseen <mailofyaseen@gmail.com>
2020-03-16 17:36:16 +0000

    Create onlick function for calc button

---
+++ docs/scripts/index.js
@@ -0,0 +1,6 @@
+const calcBtn = document.querySelector('#calc-btn');
+
+calcBtn.onclick = () => {
+  const maghrib = document.querySelector('#maghrib-time').value;
+  const fajr = document.querySelector('#fajr-time').value;
+};
